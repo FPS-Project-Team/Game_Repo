4ffab7ab891081bb47b1952addba987d291dbda3
sanjitvijay <sanjit.vijay@outlook.com>
2020-05-29 16:22:04 -0400

    Revert "Test"
    This reverts commit 401179cb67b2425414e9f5204374ef1aef1d062c.

--- FPS_Project/Assets/Scripts/PlayerShoot.cs
+++ FPS_Project/Assets/Scripts/PlayerShoot.cs
@@ -1,74 +1,50 @@
 ﻿using UnityEngine;
 using Mirror;
 
-[RequireComponent (typeof (WeaponManager))]
-public class PlayerShoot : NetworkBehaviour {
+public class PlayerShoot : NetworkBehaviour{
 
-	private const string PLAYER_TAG = "Player";
+    private const string PLAYER_TAG = "Player";
+    [SerializeField]
+    private PlayerWeapon weapon;
+    [SerializeField]
+    private GameObject weaponGFX;
+    [SerializeField]
+    private string weaponLayerName = "Weapon";
 
-	[SerializeField]
-	private Camera cam;
+    [SerializeField]
+    private Camera cam;
+    [SerializeField]
+    private LayerMask mask;
 
-	[SerializeField]
-	private LayerMask mask;
+    void Start (){
+        if(cam == null){
+            Debug.LogError("PlayerShoot: No camera referenced");
+            this.enabled = false; 
+        }
 
-	private PlayerWeapon currentWeapon;
-	private WeaponManager weaponManager;
+        weaponGFX.layer = LayerMask.NameToLayer(weaponLayerName);
+    }
 
-	void Start ()
-	{
-		if (cam == null)
-		{
-			Debug.LogError("PlayerShoot: No camera referenced!");
-			this.enabled = false;
-		}
+    void Update(){
+        if(Input.GetButtonDown("Fire1")){
+            Shoot();
+        }
+    }
 
-		weaponManager = GetComponent<WeaponManager>();
-	}
-
-	void Update ()
-	{
-		currentWeapon = weaponManager.GetCurrentWeapon();
-
-		if (currentWeapon.fireRate <= 0f)
-		{
-			if (Input.GetButtonDown("Fire1"))
-			{
-				Shoot();
-			}
-		} else
-		{
-			if (Input.GetButtonDown("Fire1"))
-			{
-				InvokeRepeating("Shoot", 0f, 1f/currentWeapon.fireRate);
-			} else if (Input.GetButtonUp ("Fire1"))
-			{
-				CancelInvoke("Shoot");
-			}
-		}
-	}
-
-	[Client]
-	void Shoot ()
-	{
-		RaycastHit _hit;
-		if (Physics.Raycast(cam.transform.position, cam.transform.forward, out _hit, currentWeapon.range, mask) )
-		{
-			if (_hit.collider.tag == PLAYER_TAG)
-			{
-				CmdPlayerShot(_hit.collider.name, currentWeapon.damage);
-			}
-		}
-
-	}
-
-	[Command]
-	void CmdPlayerShot (string _playerID, int _damage)
-	{
-		Debug.Log(_playerID + " has been shot.");
+    [Client]
+    void Shoot(){
+        RaycastHit _hit;
+        if(Physics.Raycast(cam.transform.position, cam.transform.forward, out _hit, weapon.range, mask)){
+            if(_hit.collider.tag == PLAYER_TAG){
+                CmdPlayerShot(_hit.collider.name, weapon.damage);
+            }
+        }
+    }
+    [Command]
+    void CmdPlayerShot(string _playerID,  int _damage){
+        Debug.Log(_playerID + " has been shot.");
 
         Player _player = GameManager.GetPlayer(_playerID);
         _player.RpcTakeDamage(_damage);
-	}
-
+    }
 }
